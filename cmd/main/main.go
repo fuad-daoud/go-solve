56fb9bdfe359865e7dba0dbe338094f1be896cd4
fuad <fuad.daoud@icloud.com>
2024-08-14 14:36:42 +0300

    lookedup the editorial for this one XD

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -26,43 +25,42 @@ func Solve(reader io.Reader, writer bufio.Writer) {
 	w := &writer
 	var cases int
 	fmt.Fscanf(reader, "%d\n", &cases)
+	pre_solve()
 	for testCaseNumber := 0; testCaseNumber < cases; testCaseNumber++ {
 		solving(reader, w, testCaseNumber)
 	}
 
 }
 
+const MAX = 200_000_7
+
+var a, sum [MAX]int
+
+func steps(x int) int {
+	counter := 0
+	for {
+		if x < 1 {
+			break
+		}
+		x /= 3
+		counter++
+	}
+	return counter
+}
+
+func pre_solve() {
+	a = [MAX]int{}
+	sum = [MAX]int{}
+	for index := 1; index < MAX; index++ {
+		a[index] = steps(index)
+		sum[index] = sum[index-1] + a[index]
+	}
+}
+
+// lookuped the editorial
 func solving(reader io.Reader, w *bufio.Writer, testCaseNumber int) {
-	var s, t string
-
-	fmt.Fscanf(reader, "%s\n", &s)
-	fmt.Fscanf(reader, "%s\n", &t)
-	index_of_target := 0
-	for index := range s {
-		if index_of_target < len(t) && (s[index] == t[index_of_target] || s[index] == '?') {
-			index_of_target++
-		}
-	}
-	if index_of_target == len(t) {
-		fmt.Fprintf(w, "YES\n")
-		answer := strings.Builder{}
-		index_of_target := 0
-		for index := range s {
-			if '?' == s[index] && index_of_target < len(t) {
-				answer.WriteByte(t[index_of_target])
-				index_of_target++
-			} else if '?' == s[index] {
-				answer.WriteByte('f')
-			} else {
-				if index_of_target < len(t) && s[index] == t[index_of_target] {
-					index_of_target++
-				}
-				answer.WriteByte(s[index])
-			}
-		}
-		fmt.Fprintf(w, "%s\n", answer.String())
-
-	} else {
-		fmt.Fprintf(w, "NO\n")
-	}
+	var left, right int
+	fmt.Fscanf(reader, "%v %v\n", &left, &right)
+	answer := sum[right] - sum[left-1] + a[left]
+	fmt.Fprintf(w, "%v\n", answer)
 }
