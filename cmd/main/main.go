cc0cef28e901052ce916df94d7615c91cb195514
fuad <fuad.daoud@icloud.com>
2024-08-18 23:43:31 +0300

    trying to solve div 2 c failing on test case 2

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,74 +5,128 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	writer := bufio.NewWriter(os.Stdout)
 
-	Solve(reader, *writer)
+	solver := Solver{reader, writer}
+	solver.Solve()
 }
 
-func Solve(reader io.Reader, writer bufio.Writer) {
+type Solver struct {
+	reader io.Reader
+	writer *bufio.Writer
+}
+
+func (solver Solver) Solve() {
+	r := solver.reader
+	w := solver.writer
+
 	defer func(writer *bufio.Writer) {
 		err := writer.Flush()
 		if err != nil {
+			fmt.Print("faced an error flushing " + err.Error())
 			panic(err)
 		}
-	}(&writer)
+	}(w)
 
-	w := &writer
 	var cases int
-	fmt.Fscanf(reader, "%d\n", &cases)
-	pre_solve()
+	fmt.Fscanf(r, "%d\n", &cases)
+	solver.pre_solve()
 	for testCaseNumber := 0; testCaseNumber < cases; testCaseNumber++ {
-		solving(reader, w, testCaseNumber)
+		solver.solveCase(testCaseNumber)
 	}
 
 }
 
-func pre_solve() {
+func (solver Solver) pre_solve() {
 }
 
-func solving(reader io.Reader, w *bufio.Writer, testCaseNumber int) {
-	var n, q int
-	fmt.Fscanf(reader, "%v %v\n", &n, &q)
+// looked up the toturial
+func (solver Solver) solveCase(case_number int) {
 
-	var a, b string
-	fmt.Fscanf(reader, "%v\n%v\n", &a, &b)
+	r := solver.reader
+	w := solver.writer
 
-	a_freqs := make([][]int, n+1)
-	b_freqs := make([][]int, n+1)
+	var n, k int
 
-	a_freqs[0] = make([]int, 26)
-	b_freqs[0] = make([]int, 26)
-	for index := 1; index <= n; index++ {
-		a_freqs[index] = make([]int, 26)
-		b_freqs[index] = make([]int, 26)
+	fmt.Fscanf(r, "%d %d\n", &n, &k)
 
-		for c_index := 0; c_index < 26; c_index++ {
-			a_freqs[index][c_index] = a_freqs[index-1][c_index]
-			b_freqs[index][c_index] = b_freqs[index-1][c_index]
-		}
-		a_freqs[index][a[index-1]-'a']++
-		b_freqs[index][b[index-1]-'a']++
+	numbers := make([]struct {
+		first  int64
+		second int64
+	}, n)
+
+	for index := 0; index < int(n); index++ {
+		fmt.Fscanf(r, "%d", &numbers[index].first)
 	}
-	for query := 0; query < q; query++ {
-		var left, right int
-		fmt.Fscanf(reader, "%v %v\n", &left, &right)
+	fmt.Fscanf(r, "\n")
+	for index := 0; index < int(n); index++ {
+		fmt.Fscanf(r, "%d", &numbers[index].second)
+	}
+	fmt.Fscanf(r, "\n")
+	sort.Slice(numbers, func(a, b int) bool {
+		return numbers[a].first < numbers[b].first
+	})
+	var answer int64 = 0
 
-		answer := 0
+	for index := 0; index < int(n); index++ {
+		if numbers[index].second == 1 {
+			var median int64
 
-		for index := 0; index < 26; index++ {
-			a_char_freqs := a_freqs[right][index] - a_freqs[left-1][index]
+			if index < int(n)/2 {
+				median = numbers[n/2].first
+			} else {
+				median = numbers[(n-2)/2].first
+			}
+			var current_answer int64 = median + int64(k) + numbers[index].first
 
-			b_char_freqs := b_freqs[right][index] - b_freqs[left-1][index]
-
-			if b_char_freqs > a_char_freqs {
-				answer += b_char_freqs - a_char_freqs
+			if current_answer > answer {
+				answer = current_answer
 			}
 		}
-		fmt.Fprintf(w, "%v\n", answer)
 	}
+
+	var left, right int64 = 0, 2_000_000_000
+
+	for {
+		if left >= right {
+			break
+		}
+		var mid int64 = (1 + right + left) / 2
+		numbers_more_than_or_equal_mid := 0
+		smaller_numbers := make([]int, 0)
+		for index := 0; index < int(n)-1; index++ {
+			if numbers[index].first >= mid {
+				numbers_more_than_or_equal_mid++
+			} else if numbers[index].second == 1 {
+				smaller_numbers = append(smaller_numbers, int(mid-numbers[index].first))
+			}
+		}
+		sort.Sort(sort.Reverse(sort.IntSlice(smaller_numbers)))
+		temp_k := k
+		for _, value := range smaller_numbers {
+			if temp_k >= value {
+				temp_k -= value
+				numbers_more_than_or_equal_mid++
+			}
+		}
+		if numbers_more_than_or_equal_mid >= (n+1)/2 {
+			left = mid
+		} else {
+			right = mid - 1
+		}
+	}
+
+	current_answer := numbers[n-1].first + left
+
+	if current_answer > answer {
+		answer = current_answer
+	}
+
+	fmt.Fprintf(w, "%d\n", answer)
+
 }
--- cmd/main/main_test.go
+++ cmd/main/main_test.go
@@ -15,18 +15,19 @@ func Test(t *testing.T) {
 	fmt.Printf("%+v\n", testFiles)
 
 	for _, testFile := range testFiles {
-		t.Run(testFile.name, func(test *testing.T) {
+		t.Run(testFile.name, func(t *testing.T) {
 			fmt.Println("Starting test", testFile.name)
 			ValidateFileEmpty(testFile.input)
 			ValidateFileEmpty(testFile.expected)
 
-			Solve(inputReader(testFile), *outputWriter(testFile))
-			validateOutput(testFile.output, testFile.expected, test)
+			solver := Solver{reader: inputReader(testFile), writer: outputWriter(testFile)}
+			solver.Solve()
+			validateOutput(testFile.output, testFile.expected, t)
 		})
 	}
 }
 
-func validateOutput(output, expected string, test *testing.T) {
+func validateOutput(output, expected string, t *testing.T) {
 	outputFileLines := readLines(output)
 	expectedFileLines := readLines(expected)
 
@@ -34,12 +35,12 @@ func validateOutput(output, expected string, test *testing.T) {
 	for i := 0; i < n; i++ {
 		if outputFileLines[i] != expectedFileLines[i] {
 			_, _ = fmt.Printf("failed at line %d: expected %q, got %q\n", i+1, expectedFileLines[i], outputFileLines[i])
-			test.Fail()
+			t.Fail()
 		}
 	}
 	if len(outputFileLines) != len(expectedFileLines) {
 		_, _ = fmt.Printf("file line count mismatch, outputFileLines: %d, expectedFileLines: %d\n", len(outputFileLines), len(expectedFileLines))
-		test.Fail()
+		t.Fail()
 	}
 }
 
